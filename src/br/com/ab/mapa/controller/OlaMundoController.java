3ed1cd54d426c2cb965c5851e69869388c4f684b
arturbernardo <artur.bernardo.k@gmail.com>
2015-10-03 20:46:58 -0300

    Conexão com a base.

--- src/br/com/ab/mapa/controller/OlaMundoController.java
+++ src/br/com/ab/mapa/controller/OlaMundoController.java
@@ -1,13 +1,21 @@
 package br.com.ab.mapa.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import br.com.ab.mapa.dao.PedidoDao;
+
+
 @Controller
 public class OlaMundoController {
+		
 	@RequestMapping("/olaMundoSpring")
 	public String execute() {
 		System.out.println("Executando a lógica com Spring MVC");
+		PedidoDao pedidoDao = new PedidoDao();
+		pedidoDao.testeSQL();
+
 		return "ok4";
 	}
 }
